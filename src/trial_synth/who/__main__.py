1c5034780271d6230c4d63f34d1824f86fb98760
Tomo Oga <oga.t@northeastern.edu>
2024-06-28 14:51:51 -0400

    moved ensure_df()

--- src/trial_synth/who/__main__.py
+++ src/trial_synth/who/__main__.py
@@ -10,23 +10,13 @@ import click
 import pandas as pd
 
 from .config import Config
-from .fetch import load_saved_pickled_data
 from .process import Processor
 from .store import store_dataframe_as_flat_file
-from .transform import transform_csv_data
 from .util import ensure_output_directory_exists
+from .transform import ensure_df
 
 CONFIG = Config()
 logger = logging.getLogger(__name__)
-
-
-def ensure_df() -> pd.DataFrame:
-    if CONFIG.parsed_pickle_path.is_file():
-        return load_saved_pickled_data(CONFIG.parsed_pickle_path)
-
-    df = transform_csv_data(CONFIG.csv_path)
-    df.to_pickle(CONFIG.parsed_pickle_path)
-    return df
 
 
 @click.command()
