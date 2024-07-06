c4ab221eef3b198807cb242328f5da174c04b363
Tomo Oga <oga.t@northeastern.edu>
2024-07-06 13:29:20 -0400

    created rest api models to encapsulate trial information

--- src/trial_synth/base/rest_api_response_models.py
+++ src/trial_synth/base/rest_api_response_models.py
@@ -1,36 +1,52 @@
 from pydantic import BaseModel, Field
 
 
+class Mesh(BaseModel):
+    """Stores MeSH term and ID"""
+    term: str
+    mesh_id: str = Field(alias="id")
+
+
+class ID(BaseModel):
+    """Stores curie of trial, with title"""
+    id: str = Field(alias='id')
+    title: str = Field(alias='title')
+    secondary_ids: list[str] = Field(default=[])
+
+
 class SecondaryID(BaseModel):
     """Stores Secondary ID"""
     id_type: str = Field(alias="type")
     secondary_id: str = Field(alias="id")
 
 
-class IDModule(BaseModel):
-    """Stores curie of trial, with title and secondary ID curies"""
-    id: str = Field(alias='id')
-    title: str = Field(alias='title')
-    secondary_ids: str = Field(alis='secondaryIDs', default=[])
-
-
-class ConditionsModule(BaseModel):
+class Conditions(BaseModel):
     """Stores conditions of the trial"""
-    conditions: list[str] = Field(default=[])
+    conditions: list[Mesh] = Field(default=[])
 
 
-class Intervention(BaseModel):
+class Interventions(BaseModel):
     """Represents an intervention in a clinical trial."""
-    name: str = Field(default=None)
-    intervention_type: str = Field(alias="type")
+    interventions: list[Mesh] = Field(default=[])
 
 
-class ArmsInterventionsModule(BaseModel):
-    """Stores arms and interventions of the trial."""
-    arms_interventions: list[Intervention] = Field(alias="interventions", default=[])
+class DesignInfo(BaseModel):
+    design_purpose: str = Field(alias='purpose', default=None)
+    design_allocation: str = Field(alias='allocation', default=None)
+    design_masking: str = Field(alias='masking', default=None)
+    design_assignment: str = Field(alias='assignment', default=None)
 
 
-class Mesh(BaseModel):
-    """Stores MeSH term and ID"""
-    term: str
-    mesh_id: str = Field(alias="id")
+class Outcome(BaseModel):
+    primary_outcome: str = Field(alias='primary')
+    secondary_outcome: str = Field(alias='secondary', default=None)
+
+
+class BaseTrial(BaseModel):
+    id: ID
+    study_type: str
+    design: DesignInfo
+    conditions: Conditions
+    interventions: Interventions
+    outcome: Outcome
+
