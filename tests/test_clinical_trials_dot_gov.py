6893bbe24ab7acc49e3e9c460fab50d473abf39f
Tomo Oga <oga.t@northeastern.edu>
2024-07-29 11:05:27 -0400

    changed tests for compatibility"

--- tests/test_clinical_trials_dot_gov.py
+++ tests/test_clinical_trials_dot_gov.py
@@ -3,7 +3,7 @@ import os
 from pydantic import ValidationError
 import pytest
 
-from trial_synth.clinical_trials_dot_gov import (
+from src.trialsynth.clinical_trials_dot_gov import (
     config,
     fetch
 )
@@ -19,11 +19,10 @@ def test_stability():
     Get sample data from the live API and validate it using its data model.
     """
 
-    configuration = config.Config()
-    json_data = fetch.send_request(configuration.api_url, configuration.api_parameters)
-    studies = json_data.get("studies", [])
+    configuration = config.CTConfig()
     try:
-        _ = fetch.flatten_data(studies)
+        fetch.CTFetcher(configuration)._read_next_page()
     except ValidationError as exc:
         pytest.fail(f"Unexpected error while flattening API response data: {exc}")
+
 
